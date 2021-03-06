2b1d971dd3e38fe6b1b9fd4e281ec1e5a7df2e7c
Mr. Hacker <rish@localhost.localdomain>
2021-03-06 19:33:17 +0530

    search render page

--- src/SearchPage.jsx
+++ src/SearchPage.jsx
@@ -1,13 +1,69 @@
 import React from 'react';
 import './SearchPage.css';
 import { useStateValue } from './StateProvider';
+//import useGoogleSearch from './useGoogleSearch';
+import Response from './response';
+import logo from './img/googlelogo.png'
+import {Link} from 'react-router-dom'
+
+import Search from './Search';
+import SearchIcon from '@material-ui/icons/Search';
+import DescriptionIcon from '@material-ui/icons/Description';
+import ImageIcon from '@material-ui/icons/Image';
+import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+import RoomIcon from '@material-ui/icons/Room';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+
 
 function SearchPage(){
   const [{term }, dispatch] = useStateValue();
+//  const {data } = useGoogleSearch(term);
+const data = Response;
+  console.log(data);
   return (
     <div className="searchPage">
       <div className="searchPage__header">
-        <h1>{term}</h1>
+        <Link to="/">
+          <img
+            className="searchPage__logo"
+            src={logo}
+          />
+        </Link>
+        <div className="searchPage__headerBody">
+          <Search hideButtons />
+
+          <div className="searchPage__options">
+            <div className="searchPage__optionsLeft">
+              <div className="searchPage__option">
+                <SearchIcon />
+                <Link to="/all">All</Link>
+              </div>
+              <div className="searchPage__option">
+                <DescriptionIcon />
+                <Link to="/all">News</Link>
+              </div>
+              <div className="searchPage__option">
+                <ImageIcon />
+                <Link to="/all">Images</Link>
+              </div>
+              <div className="searchPage__option">
+                <LocalOfferIcon />
+                <Link to="/all">shopping</Link>
+              </div>
+              <div className="searchPage__option">
+                <RoomIcon />
+                <Link to="/all">maps</Link>
+              </div>
+              <div className="searchPage__option">
+                <MoreVertIcon />
+                <Link to="/all">more</Link>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
       </div>
       <div className="searchPage__result">
 
---
+++ src/keys.js
@@ -0,0 +1,6 @@
+export const API_KEY  = "AIzaSyBNCkYjIJ9Kkkco9MGR-iMCrLubLfF0nos";
+
+export default API_KEY;
+
+
+//https://developers.google.com/custom-search/v1/using_rest
